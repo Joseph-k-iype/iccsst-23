8b6161f1c24cd7572c513ac23f6565e81c7bb14a
andriyazenith05 <andriyazenith05@gmail.com>
2023-03-16 16:03:25 +0530

    updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -273,7 +273,7 @@ export default function Home() {
               </h5>
             </div>
             <div className="col m-0 p-0 text-end">
-              <h5 className="mt-4 fontControl">18 March 2023</h5>
+              <h5 className="mt-4 fontControl">25 March 2023</h5>
             </div>
           </div>
           <div className="row m-0 p-4 align-items-center">
@@ -289,7 +289,7 @@ export default function Home() {
               <h5 className="mt-4 fontControl">Last date for Camera Ready paper</h5>
             </div>
             <div className="col m-0 p-0 text-end">
-              <h5 className="mt-4 fontControl">20 April 2023</h5>
+              <h5 className="mt-4 fontControl">15 April 2023</h5>
             </div>
           </div>
           <div className="row m-0 p-4 align-items-center">
